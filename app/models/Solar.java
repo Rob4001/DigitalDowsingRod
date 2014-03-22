13e64eba5e79a558419e51737ed223c735f2d507
alistaircol <aggenkeech@gmail.com>
2014-03-22 04:43:31 +0000

    Update questions view

---
+++ app/models/Solar.java
@@ -0,0 +1,18 @@
+package models;
+
+public class Solar {
+
+	public double E;	//
+	public double A;	// Area of panels = size of roof - requlatory stuff
+	public double r;	// 
+	public double H;	//Annual 
+	public double P;
+	public double R;
+	public double potentialMargin;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
