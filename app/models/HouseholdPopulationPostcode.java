b6f94d6fc1b315456a172822ab477b54b61ad6b2
Peter Kinnaird <peter@peterkinnaird.eu>
2014-03-22 00:46:48 +0000

    Adding Model for Household postcode population and a stub for Importing the CSV into the Database;

--- app/models/HouseholdPopulationPostcode.java
+++ app/models/HouseholdPopulationPostcode.java
@@ -1,5 +1,13 @@
 package models;
 
+import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.List;
+
 import javax.persistence.*;
 import play.db.ebean.*;
 import com.avaje.ebean.*;
@@ -21,5 +29,18 @@ public class HouseholdPopulationPostcode extends Model {
 
     public static Finder<String,HouseholdPopulationPostcode> find = new Finder<String,HouseholdPopulationPostcode>(
         String.class, HouseholdPopulationPostcode.class
-    ); 
+    );
+
+    public String importFromCSV(String path){
+      try {
+        CSVReader reader = new CSVReader(new FileReader(path));
+        String [] nextLine;
+        while ((nextLine = reader.readNext()) != null) {
+            System.out.println(nextLine[0] + nextLine[1] + "etc...");
+        }
+      } catch (Exception e){
+
+      }
+      return "";
+    }
 }
